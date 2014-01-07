324c62b7f31c22c2cc01fa6f4fc8e28077a8a879
Bart van den Eijnden <bartvde@boundlessgeo.com>
2014-01-07 11:53:02 +0100

    separate out the 'generic' code, couldn't get components in a different namespace to work in Ember, so resorted to creating an alias in the application namespace for the LayerSwitcherComponent

--- js/app.js
+++ js/app.js
@@ -1,23 +1,6 @@
 App = Ember.Application.create();
 
-App.MapLayer = Ember.Object.extend({
-    title: null,
-    visible: null,
-    init: function(layer) {
-        this.title = layer.get('title');
-        this.visible = layer.get('visible');
-        this.addObserver('visible', layer, function(evt) {
-            this.set('visible', evt.get('visible'));
-        });
-        layer.on('change:title', function(evt) {
-            this.set('title', evt.target.get('title'));
-        }, this);
-        layer.on('change:visible', function(evt) {
-            this.set('visible', evt.target.get('visible'));
-        }, this);
-        delete this.layer;
-    }
-});
+App.LayerSwitcherComponent = Boundless.LayerSwitcherComponent;
 
 App.Router.map(function() {
     // put your routes here
@@ -54,37 +37,8 @@ App.IndexView = Ember.View.extend({
   }
 });
 
-App.LayerSwitcherComponent = Ember.Component.extend({
-  init: function() {
-      this._super();
-      var lc = map.getLayers();
-      // if an item is removed from the layers collection, update our view
-      lc.on('remove', function(evt) {
-          var el = evt.getElement();
-          this.model.removeObject(this.model.findBy('title', el.get('title')));
-      }, this);
-  },
-  actions: {
-    removeSelected: function(item) {
-        var idx = this.model.indexOf(item);
-        if (idx !== -1) {
-            map.getLayers().removeAt(idx);
-        }
-        this.model.removeObject(item);
-    }
-  }
-});
-
-App.layersToModel = function(layers) {
-    var model = [];
-    for (var i =0, ii=layers.length; i<ii; ++i) {
-        model.push(new App.MapLayer(layers[i]));
-    }
-    return model;
-};
-
 App.IndexRoute = Ember.Route.extend({
     model: function() {
-        return App.layersToModel(map.getLayers().getArray());
+        return Boundless.layersToModel(map.getLayers().getArray());
     }
 });
---
+++ js/boundless.js
@@ -0,0 +1,51 @@
+Boundless = Ember.Namespace.create({
+  VERSION: '0.1.0'
+});
+
+Boundless.MapLayer = Ember.Object.extend({
+    title: null,
+    visible: null,
+    init: function(layer) {
+        this.title = layer.get('title');
+        this.visible = layer.get('visible');
+        this.addObserver('visible', layer, function(evt) {
+            this.set('visible', evt.get('visible'));
+        });
+        layer.on('change:title', function(evt) {
+            this.set('title', evt.target.get('title'));
+        }, this);
+        layer.on('change:visible', function(evt) {
+            this.set('visible', evt.target.get('visible'));
+        }, this);
+        delete this.layer;
+    }
+});
+
+Boundless.LayerSwitcherComponent = Ember.Component.extend({
+  init: function() {
+      this._super();
+      var lc = map.getLayers();
+      // if an item is removed from the layers collection, update our view
+      lc.on('remove', function(evt) {
+          var el = evt.getElement();
+          this.model.removeObject(this.model.findBy('title', el.get('title')));
+      }, this);
+  },
+  actions: {
+    removeSelected: function(item) {
+        var idx = this.model.indexOf(item);
+        if (idx !== -1) {
+            map.getLayers().removeAt(idx);
+        }
+        this.model.removeObject(item);
+    }
+  }
+});
+
+Boundless.layersToModel = function(layers) {
+    var model = [];
+    for (var i =0, ii=layers.length; i<ii; ++i) {
+        model.push(new Boundless.MapLayer(layers[i]));
+    }
+    return model;
+};
